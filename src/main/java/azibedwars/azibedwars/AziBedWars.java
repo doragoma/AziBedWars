d881950a95846f72e520954d23f28fc8f750ecd3
shinitaichan <41235612+doragoma@users.noreply.github.com>
2021-07-28 23:23:35 +0900

    plugin data commit

---
+++ src/main/java/azibedwars/azibedwars/AziBedWars.java
@@ -0,0 +1,11 @@
+package azibedwars.azibedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AziBedWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
